b48d7b122cdb1c2409ce77641f66788d956fcb1f
Kseniya Biarezina <ksenia-2010@tut.by>
2023-06-12 18:04:41 +0300

    fix: get new api key

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '3e9ae2059d914db4993d4e693cec9231', // получите свой ключ https://newsapi.org/ 3e9ae2059d914db4993d4e693cec9231
+      apiKey: '49df89171f2841a9995068b7b7023a08',
     });
   }
 }
