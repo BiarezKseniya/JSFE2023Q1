3f2750699c169e614a815d96312a4ef929ec4a44
Kseniya Biarezina <ksenia-2010@tut.by>
2023-06-13 17:45:19 +0300

    fix: update proxy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: '49df89171f2841a9995068b7b7023a08',
     });
   }
