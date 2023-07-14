4dcc0407d365b88c7ddbfe4639b19d5eb6a91d50
Kseniya Biarezina <ksenia-2010@tut.by>
2023-07-14 11:31:02 +0300

    feat: implement car adding

--- async-race/src/view/main-view/garage-view/garage-view.ts
+++ async-race/src/view/main-view/garage-view/garage-view.ts
@@ -8,6 +8,8 @@ export class GarageView extends View {
 
   public garageSection: GarageSectionView;
 
+  public cars: TrackView[];
+
   constructor() {
     const params: ViewParams = {
       tag: 'div',
@@ -17,17 +19,42 @@ export class GarageView extends View {
 
     this.controlsSection = new ControlsSectionView();
     this.garageSection = new GarageSectionView();
+    this.cars = [];
 
     this.configureView();
     this.createCar({ name: CarParams.defaultName, color: CarParams.defaultColor });
   }
 
   public configureView(): void {
+    this.controlsSection.carCreateBtn?.setCallback(() => {
+      const { name, color } = this.getNameColor();
+      this.createCar({ name, color });
+    });
+
     this.viewElementCreator.addInnerElement(this.controlsSection.getHtmlElement());
     this.viewElementCreator.addInnerElement(this.garageSection.getHtmlElement());
   }
 
-  private createCar({ name, color }: { name: string; color: string }): void {
-    this.garageSection.carsWrap?.addInnerElement(new TrackView(name, color).getHtmlElement());
+  private createCar({ name, color }: { name: string; color: string } = this.getNameColor()): void {
+    const newCar = new TrackView(name, color);
+    this.garageSection.carsWrap?.addInnerElement(newCar.getHtmlElement());
+    this.cars.push(newCar);
+    console.log(this.cars);
+  }
+
+  private getNameColor(): { name: string; color: string } {
+    const inputName = this.controlsSection.carCreateNameInput?.getElement();
+    const inputColor = this.controlsSection.carCreateColorInput?.getElement();
+
+    if (!(inputName instanceof HTMLInputElement) || !(inputColor instanceof HTMLInputElement)) {
+      throw new Error('There is no instance of HTMLInputElement');
+    }
+
+    let name = inputName.value;
+    if (name === '') {
+      name = CarParams.placeholderName;
+    }
+
+    return { name, color: inputColor.value };
   }
 }
